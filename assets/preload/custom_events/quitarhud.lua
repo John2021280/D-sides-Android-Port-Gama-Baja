2a75f29b7a881c2ff860df2a53fa63543427e41d
JohnXD <106628166+Joaocoder2804@users.noreply.github.com>
2022-08-30 10:19:16 -0300

    Add files via upload

---
+++ assets/preload/custom_events/quitarhud.lua
@@ -0,0 +1,8 @@
+function onEvent(name)
+	if name == 'quitarhud' then
+			setProperty('healthBarBG.visible', false)
+			setProperty('healthBar.visible', false)
+            setProperty('scoreTxt.visible', false)
+			setProperty('iconP1.visible', false)
+			setProperty('iconP2.visible', false)
+end end
